927f9b36b9af59d60023a0da2aa0530fa8fa697c
Mikael Lindh <mslindh@gmail.com>
2026-09-06 21:34:11 +0200

    Reset main job.

--- Sirocco.Dynamics/Main.cs
+++ Sirocco.Dynamics/Main.cs
@@ -32,52 +32,16 @@ namespace Sirocco.Dynamics
 
             var account1 = new Account()
             {
-                Name = "Peter Stuff",
-                Notes = new List<Note> {
-                    new Note() { Text = "Stuffed note 1" },
-                    new Note() { Text = "Another stuffed note" }
-                },
-                Contacts = new List<Contact>
-                {
-                    new Contact { Name = "Contact A", PhoneNumber = "111-222-3333" },
-                    new Contact { Name = "Contact B", PhoneNumber = "444-555-6666" }
-                }
+                Name = "Parent Account",
             };
 
             var account2 = new Account()
             {
-                Name = "Henry Winkler",
-                Notes = new List<Note> {
-                    new Note() { Text = "Winkly note 1" },
-                    new Note() { Text = "Another winkly note" },
-                    new Note() { Text = "And a third one" }
-                }
+                Name = "Under account",
             };
 
             var account1Id = _accountRepository.Create(account1);
             var account2Id = _accountRepository.Create(account2);
-
-            account1 = _accountRepository.GetById(account1Id);
-            account2 = _accountRepository.GetById(account2Id);
-
-            account2.Name = "Henry Winkler (updated)";
-            account2.Parent = account1;
-            account2.Contacts = new List<Contact>
-            {
-                new Contact { Name = "Contact 1", PhoneNumber = "123-456-7890" },
-                new Contact { Name = "Contact 2", PhoneNumber = "987-654-3210" }
-            };
-
-            _accountRepository.Update(account2);
-
-            account2 = _accountRepository.GetById(account2Id);
-
-            if(account2.Parent != null)
-            {
-                _logger.LogInformation($"{account2.Name} has parent.");
-            }
-            
-            _logger.LogInformation($"{account2.Name}:{string.Join(",", account2.Notes.Select(n => n.Text))} ; {string.Join(",", account2.Contacts.Select(c => c.Name))}");
 
             return Task.CompletedTask;
         }
