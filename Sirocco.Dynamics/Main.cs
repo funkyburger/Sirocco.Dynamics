7ffac82c5094794b5c78ba8429cc727c3cfbad8d
Mikael Lindh <mslindh@gmail.com>
2026-09-06 22:05:01 +0200

    Added contacts.

--- Sirocco.Dynamics/Main.cs
+++ Sirocco.Dynamics/Main.cs
@@ -43,7 +43,11 @@ namespace Sirocco.Dynamics
             var account1Id = _accountRepository.Create(account1);
             var account2Id = _accountRepository.Create(account2);
 
+            // 7. Create two accounts and let the first one be the parent of the second one.
             SetParentRelation(account1Id, account2Id);
+
+            // 8. Create two contacts and associate the first one with the first account and the second one with the second account
+            SetContacts(account1Id, account2Id);
 
             LogAccountDetails(_accountRepository.GetById(account1Id));
             LogAccountDetails(_accountRepository.GetById(account2Id));
@@ -61,12 +65,23 @@ namespace Sirocco.Dynamics
             _accountRepository.Update(account2);
         }
 
+        private void SetContacts(Guid account1Id, Guid account2Id)
+        {
+            var account1 = _accountRepository.GetById(account1Id);
+            var account2 = _accountRepository.GetById(account2Id);
+
+            account1.Contacts.Add(new Contact() { Name = "Contact 1", PhoneNumber = "123-456-7890" });
+            account2.Contacts.Add(new Contact() { Name = "Contact 2", PhoneNumber = "987-654-3210" });
+
+            _accountRepository.Update(account1);
+            _accountRepository.Update(account2);
+        }
+
         private void LogAccountDetails(Account account)
         {
             _logger.LogInformation($"Account ID: {account.Id}, Name: {account.Name}, Parent:{account.Parent?.Name} \n"
-                + $"Notes:{string.Join(", ", account.Notes.Select(n => $"'{n.Text}'"))}");
-                //+ $"Contacts:{string.Join(", ", $"'{account.Notes}'")}");
-
+                + $"Notes:{string.Join(", ", account.Notes.Select(n => $"'{n.Text}'"))} \n"
+                + $"Contacts:{string.Join(", ", account.Contacts.Select(c => $"'{c.Name} ({c.PhoneNumber})'"))}");
         }
     }
 }
