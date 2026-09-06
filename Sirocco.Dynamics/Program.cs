25b2579024719689cc01d03d0dcc1ca4fa10ffe3
Mikael Lindh <mslindh@gmail.com>
2026-09-06 12:31:35 +0200

    Added console logging.

--- Sirocco.Dynamics/Program.cs
+++ Sirocco.Dynamics/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 
 var services = new ServiceCollection();
 services.AddScoped<IMain, Main>();
+services.AddLogging(configure => configure.AddConsole());
 var serviceProvider = services.BuildServiceProvider();
 
 await serviceProvider.GetService<IMain>()!.Run();
