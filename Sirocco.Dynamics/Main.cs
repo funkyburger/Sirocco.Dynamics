6ee6779c8c20429237e1e425f74abb61a8d93669
Mikael Lindh <mslindh@gmail.com>
2026-09-06 12:30:27 +0200

    Added main work stub.

---
+++ Sirocco.Dynamics/Main.cs
@@ -0,0 +1,30 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sirocco.Dynamics
+{
+    internal interface IMain
+    {
+        Task Run();
+    }
+
+    internal class Main : IMain
+    {
+        private readonly ILogger<Main> _logger;
+
+        public Main(ILogger<Main> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Run()
+        {
+            _logger.LogInformation("Job started.");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Sirocco.Dynamics/Program.cs
+++ Sirocco.Dynamics/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sirocco.Dynamics;
+using Microsoft.Extensions.Logging;
+
+var services = new ServiceCollection();
+services.AddScoped<IMain, Main>();
+var serviceProvider = services.BuildServiceProvider();
+
+await serviceProvider.GetService<IMain>()!.Run();
